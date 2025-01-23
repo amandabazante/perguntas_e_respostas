380474c9063beb11e8cb0c6fcbab19d7ca14519b
amandabazante <amandabazante@outlook.com>
2025-01-23 20:29:12 -0300

    Protótipo do jogo criado

---
+++ game.py
@@ -0,0 +1,7 @@
+print("Qual a capital do Brasil? a) Brasilia b) Rio de Janeiro c) São Paulo d) Recife")
+resposta = input("Escolha a alternativa correta: ")
+if resposta == ("a"):
+  print("Você acertou")
+else:
+  print("Você errou")
+
