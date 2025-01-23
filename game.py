379e9f1c1d2de58d6dd4215dade427d23a8fcc03
amandabazante <amandabazante@outlook.com>
2025-01-23 20:40:15 -0300

    Jogo atualizado com mais pergunta

--- game.py
+++ game.py
@@ -5,3 +5,10 @@ if resposta == ("a"):
 else:
   print("Você errou")
 
+print("Quantos dentes temos na boca? a) 25 b) 30 c) 32 d) 35")
+resposta = input("Escolha a alternativa correta: ")
+if resposta == ("c"):
+  print("Você acertou")
+else:
+  print("Você errou")
+  
