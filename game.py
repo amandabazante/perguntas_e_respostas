b7742679699602c2e17b760df9f52ed22034186a
amandabazante <amandabazante@outlook.com>
2025-01-23 21:27:39 -0300

    Protótipo atualizado com pontuação

--- game.py
+++ game.py
@@ -11,4 +11,25 @@ if resposta == ("c"):
   print("Você acertou")
 else:
   print("Você errou")
-  
+
+
+print("Olá, seja bem vindo!")
+print("Cada pergunta terá um valor determinado!")
+print("Segue os valores:")
+print("A primeira pergunta vale 20 pontos!")
+print("A segunda pergunta vale 10 pontos!")
+
+perguntas = ["Qual a capital do Brasil?" , "Quantos dentes temos na boca?"]
+respostas = ["Brasília" , "32"]
+pontuacao = 0
+respostas_corretas = 0
+
+for pergunta in perguntas:
+    print(pergunta)
+    resposta_usuario = str(input('Qual sua resposta?: '))
+    for resposta in respostas:
+        if resposta_usuario == resposta:
+            respostas_corretas += 1
+pontos = respostas_corretas * 20 + 10
+
+print('Você acertou {} perguntas e sua pontuação é {}'.format(respostas_corretas, pontos))
